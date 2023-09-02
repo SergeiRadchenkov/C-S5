bc9319da2760db3086d6adfc5637c9bf81684ee1
Sergei <radchenkovsa@yandex.ru>
2023-09-02 12:51:24 +0300

    Case1

--- case2/Program.cs
+++ case2/Program.cs
@@ -3,3 +3,36 @@
 соответствующие отрицательные, и наоборот.
 [-4, -8, 8, 2] -> [4, 8, -8, -2] */
 
+int InputNum(string message){
+    System.Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void FillArray(int[] newArray){
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        newArray[i] = new Random().Next(-9, 10);
+    }
+}
+
+void Trade(int[] newArray){
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        newArray[i] *= -1;
+    }
+}
+
+void PrintArray(int[] newArray){
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        System.Console.Write(newArray[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+int size = InputNum("Введите размер массива: ");
+int[] newArray = new int [size];
+FillArray(newArray);
+PrintArray(newArray);
+Trade(newArray);
+PrintArray(newArray);
