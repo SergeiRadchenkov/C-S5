6d1abd29b4b2142dae6f51941ac929c27af5c03d
Sergei <radchenkovsa@yandex.ru>
2023-09-02 13:00:30 +0300

    Case1

--- case1/Program.cs
+++ case1/Program.cs
@@ -32,8 +32,10 @@ void FillArray(int[] arr)
 
 void PrintArray(int[] arr)
 {
-    for (int i = 0; i < arr.Length; i++)
-        Console.Write(arr[i] + " ");
+    Console.Write("[ ");
+    for (int i = 0; i < arr.Length -1; i++)
+        Console.Write(arr[i] + ", ");
+        Console.Write(arr[arr.Length -1] + " ]");
     Console.WriteLine();
 }
 
